722aa92536b280defda51e5d24a14169c32ac47d
Diego Sanz Villafruela <diegosanz@vaadin.com>
2018-12-21 12:56:09 +0200

    cleanup

--- src/main/java/org/vaadin/MainView.java
+++ src/main/java/org/vaadin/MainView.java
@@ -12,25 +12,6 @@ public class MainView extends VerticalLayout {
 
     public MainView() {
 
-//        VaadinSlide slideA = new VaadinSlide("400px","400px");
-//        slideA.add(new H1("Diego"));
-//        slideA.add(new Paragraph("My name is Diego"));
-//
-//        VaadinSlide slideB = new VaadinSlide("400px","400px");
-//        slideB.add(new H1("Juan"));
-//        slideB.add(new Paragraph("My name is Juan"));
-//
-//        VaadinSlide slideC = new VaadinSlide("400px","400px");
-//        slideC.add(new H1("Suvi"));
-//        slideC.add(new Paragraph("My name is Suvi"));
-//
-//        VaadinSlides presentation = new VaadinSlides(slideA,slideB,slideC);
-//        // presentation set dimentions by default height and witdth of the presentation
-//        add(presentation);
-
-//        MyPresentation vaadinSlides = new MyPresentation();
-//        add(vaadinSlides);
-
         DiegoSlides diegoSlides = new DiegoSlides();
 
         Div a = createDiv("red");
